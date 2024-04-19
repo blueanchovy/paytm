08ae7fa4eb5df0eed81a4a685d81b7f92a079f04
Manish Jha <manishjhazyx@gmail.com>
2024-04-20 02:00:07 +0530

    WEA

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.options("*", function (req, res) {
-  res.sendStatus(200);
-});
+app.options("*", cors());
 
 app.use("/api/v1", rootRouter);
 
